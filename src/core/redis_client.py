ee624b8a6f01d71b695aa59708042ec7496e8950
Eddow <tesei.edoardo997@gmail.com>
2026-07-19 19:24:22 +0200

    setup: add configuration layer

--- src/core/redis_client.py
+++ src/core/redis_client.py
@@ -1,3 +1,7 @@
 import redis.asyncio as redis_async
 
-redis_client = redis_async.from_url("redis://localhost:6379/0", decode_responses=True)
+from src.core.config import get_settings
+
+settings = get_settings()
+
+redis_client = redis_async.from_url(settings.redis_url, decode_responses=True)
